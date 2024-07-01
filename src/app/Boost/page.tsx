a1fcdecf993eae3fb6b434a8db3d7e548e4113cf
tsegay <tsegay.berhanu186@gmail.com>
2024-07-01 16:43:04 +0300

    changed background of container

--- src/app/Boost/page.tsx
+++ src/app/Boost/page.tsx
@@ -2,9 +2,8 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex flex-col px-6 items-center gap-4">
-      <div className="w-full text-white text-3xl flex text-center">Boost</div>
-      <div className="w-full  flex justify-center">hello</div>
-      <div className="w-full flex flex-col gap-4   p-2 ">
+      <div className="w-full text-white text-3xl flex justify-center">Boost</div>
+      <div className="w-full flex flex-col gap-4 ">
         <button className=" text-start p-4 bg-purple-800 bg-opacity-10  text-white font-semibold rounded-md">Multi-Tap</button>
         <button className=" text-start p-4 bg-purple-800 bg-opacity-10  text-white font-semibold rounded-md">Energy Limit</button>
         <button className=" text-start p-4 bg-purple-800 bg-opacity-10  text-white font-semibold rounded-md">Recharge Speed</button>
