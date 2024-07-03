0fdd4904b754c4078ad45a3e24e4743a321db823
tsegay <tsegay.berhanu186@gmail.com>
2024-07-03 15:11:40 +0300

    mobile first design

--- src/app/Components/AppMenu.tsx
+++ src/app/Components/AppMenu.tsx
@@ -5,7 +5,7 @@ const AppMenu = () => {
   const pathname = usePathname();    
   const style =    "bg-gray-800 rounded-sm w-16 h-14 flex flex-grow flex-shrink justify-center items-center ";
   return (
-    <div className="bg-red-950 flex gap-2 justify-between w-full px-4 pt-4 pb-12 h-28 bottom-0 fixed ">
+    <div style={{maxWidth:"352px"}} className="bg-slate-950 flex gap-2 justify-between w-full  pt-4 pb-12 h-28 bottom-0 fixed ">
       <div className={`${style} ${pathname == "/Refs" ? "opacity-50" : ""}`}>
         <Link className="text-white" href="/Refs">
           Refs
