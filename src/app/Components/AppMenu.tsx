2c58263e5cc1ddb75c5a5dd90f845e7427619824
tsegay <tsegay.berhanu186@gmail.com>
2024-07-03 15:41:44 +0300

    caluculating body width

--- src/app/Components/AppMenu.tsx
+++ src/app/Components/AppMenu.tsx
@@ -1,11 +1,22 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 const AppMenu = () => {
+
+  const [bodyWidth, setBodyWidth] = useState(window.innerWidth);
+  console.log(bodyWidth)
+
+  useEffect(() => {
+    const handleResize = () => setBodyWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   const pathname = usePathname();    
   const style =    "bg-gray-800 rounded-sm w-16 h-14 flex flex-grow flex-shrink justify-center items-center ";
   return (
-    <div style={{maxWidth:"352px"}} className="bg-slate-950 flex gap-2 justify-between w-full  pt-4 pb-12 h-28 bottom-0 fixed ">
+    <div style={{maxWidth:`${bodyWidth-32}px`}}  className="bg-slate-950 flex gap-2 justify-between w-full  pt-4 pb-16 bottom-0 fixed ">
       <div className={`${style} ${pathname == "/Refs" ? "opacity-50" : ""}`}>
         <Link className="text-white" href="/Refs">
           Refs
