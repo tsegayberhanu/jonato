4de80532fe72e0ffa785fddf4530d7799fb22f95
tsegay <tsegay.berhanu186@gmail.com>
2024-07-08 16:59:57 +0300

    adcard dialogue implementation

--- src/app/Components/AppAddCard.tsx
+++ src/app/Components/AppAddCard.tsx
@@ -1,27 +1,61 @@
 "use client";
 import React, { useState } from "react";
 import { useRef } from "react";
+import Image, { StaticImageData } from "next/image";
 interface AppAdCardProps {
-  children?: React.ReactNode;
+  adImageSrc: StaticImageData;
+  adModalTitle?: string;
+  adDescription?: string;
+  adValue?: string;
+  adCardTitle: string;
 }
-const AppAddCard = ({ children }: AppAdCardProps) => {
+const AppAddCard = ({ adCardTitle, adImageSrc }: AppAdCardProps) => {
   const modalRef = useRef<HTMLDialogElement | null>(null);
   const [showModal, setShowModal] = useState(false);
+
   const handleClick = () => {
     setShowModal(!showModal);
     showModal ? modalRef.current?.close() : modalRef.current?.showModal();
   };
+
   return (
     <>
       <dialog ref={modalRef}>
         <div
           style={{ boxShadow: "0 -8px 64px rgb(249 250 251)" }}
-          className="fixed bottom-0 left-0 flex flex-col justify-around items-center rounded-t-3xl h-4/5  border-t-white border-t-2 bg-neutral-950 w-full"
+          className="fixed bottom-0 left-0 flex flex-col gap-2 justify-around items-center p-4 rounded-t-3xl h-fit  border-t-white border-t-2 bg-neutral-950 w-full"
         >
-          <div className="text-white">data goes here</div>
-          <div>
+          <div className="flex w-full justify-end">
             <button
-              className="p-2 bg-blue-900 rounded-md w-full"
+              onClick={handleClick}
+              className="rounded-full self-end bg-slate-800 bg-opacity-20 py-4 px-6 text-xl text-white"
+            >
+              X
+            </button>
+          </div>
+          <div>
+            <p className="text-xl p-4 self-center  text-white font-semibold">
+              Green Tech Africa
+            </p>
+          </div>
+          <div className="relative w-full flex justify-center">
+            <Image
+              src={adImageSrc}
+              height={50}
+              width={100}
+              alt="Afri Logo"
+              className="bg-inherit  rounded-sm"
+            />
+          </div>
+          <div className="text-white max-w-sm p-2">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+            neque nostrum quibusdam rerum magni corporis, ratione voluptates
+            fugit repudiandae a nemo dignissimos quod! Blanditiis libero
+            deserunt asperiores rem officia quod.
+          </div>
+          <div className="w-full flex justify-center">
+            <button
+              className="p-4 w-1/2 bg-blue-900 rounded-md "
               onClick={handleClick}
             >
               continue
@@ -29,10 +63,38 @@ const AppAddCard = ({ children }: AppAdCardProps) => {
           </div>
         </div>
       </dialog>
-      <div onClick={handleClick}
-        className="bg-purple-800 bg-opacity-10 text-white  p-2 w-1/3 max-w-40 flex justify-center items-center flex-grow  rounded-md h-36"
+      <div
+        onClick={handleClick}
+        className="bg-gray-900  text-white h-fit  p-2 w-1/3  flex flex-col gap-2 justify-center items-center flex-grow  rounded-md "
       >
-        {children}
+        <div className="flex h-fit gap-4 pb-2">
+          <div className="self-center h-16 w-16">
+            <Image
+              src={adImageSrc}
+              alt="niber icon"
+              height={90}
+              width={120}
+              className="shadow-inner bg-white rounded-sm border-4 border-white"
+            ></Image>
+          </div>
+
+          <div className="flex flex-col justify-between h-16 ">
+            <div className="text-stone-100 text-sm font-semibold">
+              {adCardTitle}
+            </div>
+            <div>
+              <p className="text-slate-100 text-xs ">profit per hour</p>
+              <p className="text-white">2.51k</p>
+            </div>
+          </div>
+        </div>
+        <div className=" border-b-slate-950 border-b w-full"></div>
+
+        <div className="flex justify-around gap-2 ">
+          <div className="text-sm">lvl 0</div>
+          <div className=" border-r-slate-950 border-r"></div>
+          <div className="text-sm">120000</div>
+        </div>
       </div>
     </>
   );
