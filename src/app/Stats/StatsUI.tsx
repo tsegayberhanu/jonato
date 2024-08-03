c76c05197095cac46fd7567f89162cdd26dd8865
tsegay <tsegay.berhanu186@gmail.com>
2024-08-03 11:06:51 +0300

    removed user data from stats ui

--- src/app/Stats/StatsUI.tsx
+++ src/app/Stats/StatsUI.tsx
@@ -1,44 +1,8 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import WebApp from "@twa-dev/sdk";
-interface UserData {
-  id: number;
-  first_name: string;
-  last_name?: string;
-  username?: string;
-  language_code: string;
-  is_premium?: boolean;
-}
+import React from "react";
 const StatsUI = () => {
-  const [userData, setUserData] = useState<UserData | null>(null)
-  useEffect(() => {
-    if(typeof window !== "undefined"){
-      if (WebApp.initDataUnsafe.user) {
-        setUserData(WebApp.initDataUnsafe.user as UserData)
-      }
-    }
-  }, [])
-
   return (
     <div className="flex flex-col items-center gap-4 text-white">
       <div className="text-white text-3xl">Stats</div>
-      {userData ? (
-        <>
-          <h1 className="text-2xl font-bold mb-4">User Data</h1>
-          <ul>
-            <li>ID: {userData.id}</li>
-            <li>First Name: {userData.first_name}</li>
-            <li>Last Name: {userData.last_name || 'N/A'}</li>
-            <li>Username: {userData.username || 'N/A'}</li>
-            <li>Language Code: {userData.language_code}</li>
-            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
-          </ul>
-        </>
-      ) : (
-        <div>Loading...</div>
-      )}
-
-
       <div className="flex flex-col items-center">
         <p className="text-xl font-bold">Total Share Balance</p>
         <p className="font-thin">416.281 T</p>
@@ -66,4 +30,5 @@ const StatsUI = () => {
     </div>
   );
 };
+
 export default StatsUI;
