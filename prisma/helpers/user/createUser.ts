ec014b3b22ff45594bd330c0bdf1dcbb27ca2c07
tsegay <tsegay.berhanu186@gmail.com>
2024-11-01 15:33:58 +0300

    alias configured import

--- prisma/helpers/user/createUser.ts
+++ prisma/helpers/user/createUser.ts
@@ -1,4 +1,4 @@
-import prisma from "../../prisma";
+import prisma from "@db/prisma";
 async function createUser(data: any) {
     return prisma.user.create({
         data
--- prisma/helpers/user/getUser.ts
+++ prisma/helpers/user/getUser.ts
@@ -1,4 +1,4 @@
-import prisma from "../../prisma";
+import prisma from "@db/prisma";
 async function getUser(telegramId:number) {
     return prisma.user.findUnique({
         where: { telegramId}
