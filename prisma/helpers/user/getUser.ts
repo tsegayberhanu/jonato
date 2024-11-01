78f38672e5ff254dd88051c051f4d81926486422
tsegay <tsegay.berhanu186@gmail.com>
2024-11-01 16:37:11 +0300

    debugging: db query

--- prisma/helpers/user/getUser.ts
+++ prisma/helpers/user/getUser.ts
@@ -1,8 +1,13 @@
 import prisma from "@db/prisma";
-async function getUser(telegramId:number) {
-    return prisma.user.findUnique({
-        where: { telegramId}
+async function getUser(telegramId: number) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { telegramId },
     });
+    return user;
+  } catch (error) {
+    console.log(error)
+    return
+  }
 }
-
 export default getUser;
