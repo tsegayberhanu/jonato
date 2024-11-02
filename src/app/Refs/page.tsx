fd4e066d03aa5f659c5fddcde600bed518e8aa26
tsegay <tsegay.berhanu186@gmail.com>
2024-11-02 16:52:30 +0300

    redesign: ref pages

--- src/app/Refs/page.tsx
+++ src/app/Refs/page.tsx
@@ -1,54 +1,19 @@
-"use client"
-import React, { useContext } from "react";
+"use client";
 import { FaRegUser } from "react-icons/fa";
-import { useState } from "react";
-
-import { CoinBalanceContext } from "../Contexts/CoinBalanceContext";
+import { shareURL } from '@telegram-apps/sdk';
 
 const page = () => {
-
-  const { balance } = useContext(CoinBalanceContext)
-  const refLink = "http://localhost:3000/Refs"
-  const [copied, setCopied] = useState(false);
-  const handleClick = async () => {
-    if (refLink) {
-      try {
-        await navigator.clipboard.writeText(refLink);
-        setCopied(true);
-      } catch (error) {
-        console.error('Failed to copy text:', error);
-      } finally {
-        setTimeout(() => setCopied(false), 1000); // Reset copied state after 1 second
-      }
-    }
-
-  }
-
- 
-
   return (
-    <div className="flex flex-col gap-4">
-
-    <div className="bg-slate-700 bg-opacity-20 p-4 rounded-md flex flex-col gap-1 text-white">
-      <div className="flex justify-between">
-        <div className="text-white text-xl">{balance}</div>
-      <p className="text-xl font-bold">My Invite Link</p>
-      <button className="p-2 rounded-xl bg-stone-200 text-black" onClick={handleClick}>Copy</button>
+    <div className="flex flex-col gap-4 relative">
+      <div className="w-full flex flex-col gap-4 ">
+        <div className="text-white text-xl"> 12 Refs</div>
+        <div>
+          <FriendComponent />
+        </div>
       </div>
-      <div>
-        <p  className="bg-transparent font-thin text-xl text-wrap break-all" >
-          {refLink}
-        </p>
+      <div className="bottom-0">
+        <InviteFriend userId={BigInt(123)} />
       </div>
-    </div>
-    
-    <div className="w-full flex flex-col gap-4 ">
-      <div className="text-white text-xl"> 12 Refs</div>
-      <div>
-        <FriendComponent/>
-      </div>
-    </div>
-
     </div>
   );
 };
@@ -59,7 +24,7 @@ const FriendComponent = () => {
     <div className="text-white flex w-full justify-between gap-6 p-2 rounded-md bg-slate-500 bg-opacity-10">
       <div className="flex gap-4">
         <div className="self-center">
-        <FaRegUser className="w-8 h-8"/>
+          <FaRegUser className="w-8 h-8" />
         </div>
         <div>
           <p className="font-semibold">Izona</p>
@@ -70,10 +35,42 @@ const FriendComponent = () => {
         </div>
       </div>
 
-      <p className="self-center">
-       100000
-      </p>
-
+      <p className="self-center">100000</p>
     </div>
-  )
+  );
+};
+
+import { FaCopy } from "react-icons/fa";
+const InviteFriend = ({ userId }: RefereTelegramIdProp) => {
+  const INVITE_URL = "https://t.me/ezylBot/start";
+  const handleInviteFriend = () => {
+    const inviteLink = `${INVITE_URL}?startapp=${userId}`;
+    const shareText = `Join me on this awesome Telegram mini app!`;
+     shareURL(inviteLink, shareText);
+  };
+
+  const handleCopyInviteLink = () => {
+    const inviteLink = `${INVITE_URL}?startapp=${userId}`;
+    navigator.clipboard.writeText(inviteLink);
+    alert("Invite link copied to clipboard!");
+  };
+
+  return (
+    <div className="bg-slate-700 w-full flex flex-row gap-3 justify-between  bg-opacity-20 p-4 rounded-md">
+      <div className="flex-grow justify-center items-center bg-blue-500 rounded-md">
+        <button className=" w-full py-4" onClick={handleInviteFriend}>
+        invite a friend
+        </button>
+        </div>
+      <div className="bg-white bg-opacity-10 flex justify-center items-center rounded-md">
+        <button  className="w-full h-full px-6" onClick={handleCopyInviteLink}>
+        <FaCopy size={28} className="text-blue-500" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+interface RefereTelegramIdProp {
+  userId: bigint;
 }
